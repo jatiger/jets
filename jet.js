5536bd3f073ea00138bc3f87d3a7a76942f11343
Jay Averitt <jayaveritt@gmail.com>
2020-02-19 14:06:51 -0500

    Adding Takeoff Feature

--- jet.js
+++ jet.js
@@ -1,6 +1,10 @@
 var Jet = function (color, size) {
-    this.color = color;
-    this.size = size;
+  this.color = color;
+  this.size = size;
+
+  this.takeOff = function () {
+    console.log('lift off!');
   }
-  
-  module.exports = Jet;
+}
+
+module.exports = Jet;
